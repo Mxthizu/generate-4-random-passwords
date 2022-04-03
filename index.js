06059644d8c48b5f87afca0f48177a06be2a56b2
Mxthizu <89317118+Mxthizu@users.noreply.github.com>
2022-04-03 18:42:46 +0200

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@?!-_()";
+let passwordNumber = 4
+let passwordEl = document.querySelectorAll(".password-el")
+let passwords = []
+
+function generatePassword() {
+    let newPassword = ""
+    for (let i = 0; i < 12; i++) {
+        let generate = characters[Math.floor(Math.random() * characters.length)]
+        newPassword += generate
+    }
+    passwords.push(newPassword)
+}
+
+function generateMultiplePasswords() {
+    while (passwords.length < passwordNumber - 2) {
+        for (let i = 0; i < passwordNumber; i++) {
+            generatePassword()
+            passwordEl[i].textContent = passwords[i]
+        }   
+    }
+    passwords = []
+}
